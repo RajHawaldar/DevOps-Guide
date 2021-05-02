5b4dd2e1efdfed8d724b6c4028f876f2cb9febbb
Raj Hawaldar <rajhawaldar007@gmail.com>
2021-05-02 19:26:58 +0530

    Script to Install Chrome on VM with PowerShell

---
+++ Automation Scripts/Powershell/To-Install-Chrome-On-VM-PowerShell.ps1
@@ -0,0 +1,3 @@
+#Use PowerShell to download and install Chrome on VM machine as we don't want to open IE at all!!!
+
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
